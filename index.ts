5504ca3e05350d089c06610fcc2d7f4ffa2865d8
Filip Razek <filip.razek@protonmail.com>
2023-10-24 14:45:14 +0200

    Implement word comparison

--- index.ts
+++ index.ts
@@ -1,14 +1,23 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
+let word = chooseRandomWord();
 serve(handler);
 
 async function handler(_req: Request): Promise<Response> {
   if (!_req.body) {
     return new Response("No text found");
   }
+
   const sentValue = await readStream(_req.body);
   const text = extractParameter(sentValue, "text");
-  return new Response(text);
+
+  if (!text) {
+    return new Response("No text found");
+  }
+
+  const response = evaluateGuess(text.toLowerCase());
+
+  return new Response(response);
 }
 
 async function readStream(stream: ReadableStream<Uint8Array>) {
@@ -30,4 +39,27 @@ async function readStream(stream: ReadableStream<Uint8Array>) {
 function extractParameter(urlQuery: string, parameter: string) {
   const query = new URLSearchParams(urlQuery);
   return query.get(parameter);
+}
+
+function compareWordsSemantically(word1: string, word2: string) {
+  return Math.random();
+}
+
+function chooseRandomWord() {
+  const words = ["infrastructure", "devops", "cloud", "serverless"]
+  return randomChoice(words);
+}
+
+function randomChoice<T>(array: T[]) {
+  const index = Math.floor(Math.random() * array.length);
+  return array[index];
+}
+
+function evaluateGuess(guess: string) {
+  if (guess === word) {
+    word = chooseRandomWord();
+    return "You win! Choosing a new word...";
+  }
+  const score = compareWordsSemantically(word, guess);
+  return `You guessed ${guess}. The correct word was ${word}. Similarity score: ${Math.floor(score * 100)}%. Try again!`
 }
